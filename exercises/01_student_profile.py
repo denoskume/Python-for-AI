9cd43dfaf9514a7706cdc395d32fb55dfe8bedf3
denoskume <denoskume77@gmail.com>
2026-07-10 10:14:02 +0200

    feat: add student profile exercise

---
+++ exercises/01_student_profile.py
@@ -0,0 +1,23 @@
+# Student profile
+
+name = "Denos Kume"
+university = "Centrale Nantes"
+master = "Data Science, Signal and Image Processing"
+dream_company = "NVIDIA"
+daily_study_hours = 3
+
+current_python_level = 5
+target_python_level = 9
+remaining_python_levels = target_python_level - current_python_level
+
+print("---------- STUDENT PROFILE ----------")
+print(f"Name: {name}")
+print(f"University: {university}")
+print(f"Master: {master}")
+print(f"Dream Company: {dream_company}")
+print(f"Daily Study Hours: {daily_study_hours}")
+print(f"Current Python Level: {current_python_level}")
+print(f"Target Python Level: {target_python_level}")
+print(f"Remaining Python Levels: {remaining_python_levels}")
+print("-------------------------------------")
+
